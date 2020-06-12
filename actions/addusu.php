f67eda896e3e05685af195b2ab9e888dfed41e25
João Danilo Zucolotto <jddiedrich@gmail.com>
2020-06-12 09:07:15 -0300

    corrigido cadastro de usuario removi css redundante

--- actions/addusu.php
+++ actions/addusu.php
@@ -2,13 +2,11 @@
 	include '../config/conexao.php';
 
 	$nome = $_POST['nome'];
-	$sobrenome = $_POST['sobrenome'];
 	$email = $_POST['email'];
-	$user = $_POST['user'];
 	$senha = md5($_POST['senha']);
 	$tipou = $_POST['tipou'];	
 	$id = uniqid();	
 
-	$sql = "INSERT INTO usuario (user_id, user_nome, user_email, user_senha, hierarquia_nome) VALUES ('$id','$nome','$email','$senha','$tipou')";
+	$sql = "INSERT INTO usuario (usario_id, userio_nome, userio_email, userio_senha, hierarquia_nome) VALUES ('$id','$nome','$email','$senha','$tipou')";
 	$result = $connection->query($sql) or die ("$connection->error<br>$sql");
 	$connection->close();
