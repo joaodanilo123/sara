9ba002afc7f76d3d9b72a22b6c0d2e4f45f2d12c
João Danilo Zucolotto <jddiedrich@gmail.com>
2020-07-22 15:17:26 -0300

    Edição do ambiente

--- actions/editar_ambiente.php
+++ actions/editar_ambiente.php
@@ -1,14 +1,14 @@
 <?php
-include '../config/conexao.php';
 
-print_r($_POST);
+include '../config/conexao.php';
 
 $nome = $_POST['nome'];
 $numero = $_POST['numero'];
 $tipo_ambiente_id = $_POST['tipoa'];
 $predio = $_POST['predio'];
 $id = uniqid();
-$query = "INSERT INTO ambiente(ambiente_id, ambiente_nome, ambiente_numero, tipo_ambiente_id, predio_id) VALUES ('$id','$nome', '$numero', '$tipo_ambiente_id', '$predio')";
+$status = $_POST['status'];
+$query = "INSERT INTO ambiente(ambiente_id, ambiente_nome, ambiente_numero, tipo_ambiente_id, predio_id, ambiente_ativo) VALUES ('$id','$nome', '$numero', '$tipo_ambiente_id', '$predio', '$status')";
 $connection->query($query) or die($connection->error);
 
 if (!$connection->error) {
--- editar/ambiente.php
+++ editar/ambiente.php
@@ -27,14 +27,18 @@ $connection->close();
         <form class="needs-validation" action="../actions/cadastrar_ambiente.php" method="post" novalidate>
             <div class="row">
                 <div class="col-md-6 mb-3">
-                    <label for="nome">Nome</label>
+                    <label for="nome">
+                        <h4 class="mb-3">Nome</h4>
+                    </label>
                     <input type="text" class="form-control" name="nome" placeholder="" value="<?= $dadosAtuais['ambiente_nome'] ?>" required>
                     <div class="invalid-feedback">
                         Este campo é obrigatório.
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
-                    <label for="numero">Número</label>
+                    <label for="numero">
+                        <h4 class="mb-3">Número</h4>
+                    </label>
                     <input type="number" class="form-control" name="numero" placeholder="" value="<?= $dadosAtuais['ambiente_numero'] ?>" required>
                 </div>
             </div>
@@ -58,6 +62,7 @@ $connection->close();
                 <?php endforeach; ?>
             </div>
 
+            <h4 class="mb-3">Prédio</h4>
             <select name="predio" id="predio" class="form-control">
                 <?php foreach ($predios as $p) : ?>
                     <?php if ($p['predio_id'] == $dadosAtuais['predio_id']) : ?>
@@ -67,8 +72,27 @@ $connection->close();
                     <?php endif; ?>
                 <?php endforeach ?>
             </select>
-
-
+            <br>
+            <h4 class="mb-3">Ativo</h4>
+            <?php if ($dadosAtuais['ambiente_ativo'] == 'sim'): ?>
+                <div>
+                    <input id="status" name="status" type="radio" value="sim" checked required>
+                    <label for="status">Sim</label>
+                </div>
+                <div>
+                    <input id="status" name="status" type="radio" value="não" required>
+                    <label for="status">Não</label>
+                </div>
+            <?php else : ?>
+                <div>
+                    <input id="status" name="status" type="radio" value="sim" checked required>
+                    <label for="status">Não</label>
+                </div>
+                <div>
+                    <input id="status" name="status" type="radio" value="sim" required>
+                    <label for="status">Sim</label>
+                </div>
+            <?php endif; ?>
 
             <hr class="mb-4">
             <button class="btn btn-primary btn-lg btn-block" type="submit">Atualizar</button>
