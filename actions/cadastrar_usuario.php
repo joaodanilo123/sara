940cf82054ab7b1a13f89b123770236599d4fbb9
João Danilo Zucolotto Diedrich <jdzd@pop-os.localdomain>
2020-09-07 13:06:37 -0300

    token add on register + redirect message

--- actions/cadastrar_usuario.php
+++ actions/cadastrar_usuario.php
@@ -1,16 +1,17 @@
 <?php
-	include '../config/conexao.php';
-
+	require '../config/conexao.php';
+	
 	$nome = $_POST['nome'];
 	$email = $_POST['email'];
 	$senha = md5($_POST['senha']);
-	$tipou = $_POST['tipou'];	
+	$tipou = $_POST['tipou'];
+	$token = $tipou == 'professor' ? $_POST['token'] : '0000000000';
 	$id = uniqid();	
 
-	$sql = "INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha, hierarquia_nome) VALUES ('$id','$nome','$email','$senha','$tipou')";
+	$sql = "INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha, hierarquia_nome, usuario_token) VALUES ('$id','$nome','$email','$senha','$tipou', '$token')";
 	$result = $connection->query($sql) or die ("$connection->error<br>$sql");
 	$connection->close();
 
-	header('Location: ../painel/admin.php');
+	header('Location: ../painel/admin.php?message=Usuário+cadastrado+com+sucesso');
 
 
