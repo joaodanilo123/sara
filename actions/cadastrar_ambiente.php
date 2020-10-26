1942000318904f9a67e91443720904f71a461d2b
João Danilo Zucolotto Diedrich <jdzd@pop-os.localdomain>
2020-10-26 16:30:57 -0300

    sim

--- actions/cadastrar_ambiente.php
+++ actions/cadastrar_ambiente.php
@@ -1,18 +1,28 @@
 <?php
-include '../config/conexao.php';
 
-print_r($_POST);
+include '../config/conexao.php';
 
 $nome = $_POST['nome'];
 $numero = $_POST['numero'];
 $tipo_ambiente_id = $_POST['tipoa'];
 $predio = $_POST['predio'];
 $id = uniqid();
-$query = "INSERT INTO ambiente(ambiente_id, ambiente_nome, ambiente_numero, tipo_ambiente_id, predio_id) VALUES ('$id','$nome', '$numero', '$tipo_ambiente_id', '$predio')";
-$connection->query($query) or die($connection->error);
 
-if (!$connection->error) {
+try {
+	$query = $connection->prepare("INSERT INTO ambiente(ambiente_id, ambiente_nome, ambiente_numero, tipo_ambiente_id, predio_id) 
+	VALUES (:id, :nome, :numero, :tipo_ambiente, :predio)");
+
+	$query->bindParam(':id', $id);
+	$query->bindParam(':nome', $nome);
+	$query->bindParam(':numero', $numero);
+	$query->bindParam(':tipo_ambiente', $tipo_ambiente_id);
+	$query->bindParam(':predio', $predio);
+
+	$query->execute();
 	header('Location: ../painel/admin.php?erro=no');
-} else {
+} catch (PDOException $error) {
+	echo $error->getMessage();
 	header('Location: ../painel/admin.php?erro=erro');
 }
+
+$connection = null;
--- config/conexao.php
+++ config/conexao.php
@@ -1,3 +1,16 @@
 <?php
-$connection = new mysqli("localhost", "jdzd", "123", "sara"); 
-$connection->set_charset('utf8');
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+
+try {
+    $connection = new PDO("mysql:host=$host;dbname=sara", $user, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $error) {
+    echo "Connection failed: " . $error->getMessage();
+}
+
+
+//$connection = new mysqli("localhost", "jdzd", "123", "sara"); 
+//$connection->set_charset('utf8');
